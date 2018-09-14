425594086b445aed1bc523f2484c1b195250cd35
TomMaze <thomasmaaza123@gmail.com>
2018-09-14 14:09:32 +0200

    SPI and sensor configured.

--- Prac4_code.py
+++ Prac4_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 #Declaration of libraries 
+import spidev
 import time
 import datetime
 import os
@@ -13,7 +14,19 @@ freq =27
 stop =22
 display=23
 
-GPIO.setup(reset, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(freq, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(stop, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(display, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#setup for interrupt switches
+GPIO.setup(reset, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(freq, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(stop, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(display, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Open SPI bus
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz=1000000
+
+# Define sensor channels
+light_channel = 0
+temp_channel  = 1
+pot_channel= 2
+
